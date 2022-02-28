701ae25078f17f53e94ff30d65c218657d66f050
Bkain1 <Bkain1@student.cvtc.edu>
2022-02-28 11:14:53 -0600

    [finishes #181351243] Created color generator for card, fixed number generator to be inclusive of max

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
-// Generate a random number 0-9
+// Generate a random number 1 - max
 function genRandom(max) {
-  return Math.floor(Math.random() * max);
-  
-  
-  
+  return Math.floor(Math.random() * max + 1);
 }
 
 function genSeed() {
 
   // Create seed array
-  var seed = new Array(5);
-  var arrayLength = seed.length;
+  let seed = new Array(5);
+  let arrayLength = seed.length;
 
-  // Set a random number 0 - 9
-  for (var i = 0; i < arrayLength; i++) {
-    seed[i] = genRandom(9);
+  // Set a random number 1 - 8 to the array
+  for (let i = 0; i < arrayLength; i++) {
+    seed[i] = genRandom(8);
   }
 
-  alert(seed);
+  console.log("SEED: " + seed);
+
+  //alert(seed);
+
+  // Using array to set values for card
+  let mana = seed[0];
+  let type = seed[1];
+  let text = seed [2];
+  let power = seed[3];
+  let toughness = seed[4];
+
+  // Setting color from 1 - 6
+  let color = genRandom(6);
+  console.log("COLOR: " + color);
+  let container = document.getElementById("container");
+
+  switch(color) {
+    case 1:
+      // Colorless
+      container.style.backgroundColor = "#d3d3d3";
+      break;
+    case 2:
+      // White
+      container.style.backgroundColor = "#ffffff";
+      break;
+    case 3:
+      // Red
+      container.style.backgroundColor = "#ff3c3c";
+      break;
+    case 4:
+      // Green
+      container.style.backgroundColor= "#397a04";
+      break;
+    case 5:
+      // Blue
+      container.style.backgroundColor = "#4775ff";
+      break;
+    case 6:
+      // Black
+      container.style.backgroundColor = "#5c5c5c";
+      break;
+  }
+
 }
