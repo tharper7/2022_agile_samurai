b91797029adb677b31a3a9df6779c60cf27d966b
AJ Fischer <etherealNekryx@users.noreply.github.com>
2022-03-01 02:16:24 -0600

    [Finishes #181351275] Completed a basic text format randomizer

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function genSeed() {
 
   console.log("SEED: " + seed);
 
-  //alert(seed);
 
   // Using array to set values for card
   let mana = seed[0];
@@ -57,4 +56,135 @@ function genSeed() {
       break;
   }
 
+  // Set text format effects/keywords
+  let textBox = document.getElementById("cardText");
+  let textType = genRandom(7);
+  let textEffect = genRandom(8);
+  let textTrigger = genRandom(5);
+  let textCost = genRandom(4);
+  let textAbility = genRandom(8);
+  
+  switch(textType) {
+    case 1:
+      textType = "Creature";
+      break;
+    case 2:
+      textType = "Artifact";
+      break;
+    case 3:
+      textType = "Enchantment";
+      break;
+    case 4:
+      textType = "Instant";
+      break;
+    case 5:
+      textType = "Sorcery";
+      break;
+    case 6:
+      textType = "Planeswalker";
+      break;
+    case 7:
+      textType = "Land";
+      break;
+  }
+  
+  switch(textEffect) {
+    case 1:
+      textEffect = "draw a card";
+      break;
+    case 2:
+      textEffect = "create a 2/2 black zombie creature token";
+      break;
+    case 3:
+      textEffect = "draw two cards and then discard a card";
+      break;
+    case 4:
+      textEffect = "return a noncreature card from your graveyard to your hand";
+      break;
+    case 5:
+      textEffect = "return a target nonland permanent to it's owners hand";
+      break;
+    case 6:
+      textEffect = "exile a card from a graveyard";
+      break;
+    case 7:
+      textEffect = "add one mana of any color to your mana pool";
+      break;
+    case 8:
+      textEffect = "make target player mill 2 cards";
+      break;
+  }
+  
+  switch(textTrigger) {
+    case 1:
+      textTrigger = "dies";
+      break;
+    case 2:
+      textTrigger = "enters the battlefield";
+      break;
+    case 3:
+      textTrigger = "leaves the battlefield";
+      break;
+    case 4:
+      textTrigger = "attacks";
+      break;
+    case 5:
+      textTrigger = "deals combat damage";
+      break;
+  }
+  
+  switch(textAbility) {
+    case 1:
+      textAbility = "Deathtouch";
+      break;
+    case 2:
+      textAbility = "Lifelink";
+      break;
+    case 3:
+      textAbility = "Vigilance";
+      break;
+    case 4:
+      textAbility = "First Strike";
+      break;
+    case 5:
+      textAbility = "Double Strike";
+      break;
+    case 6:
+      textAbility = "Indestructible";
+      break;
+    case 7:
+      textAbility = "Haste";
+      break;
+    case 8:
+      textAbility = "Trample";
+      break;
+  }
+  
+  // Set text format and add it to the textbox
+  switch(text) {
+    case 1:
+      textBox.innerHTML = "Whenever you cast a " + textType + " spell, you may " + textEffect + ".";
+      break;
+    case 2:
+      textBox.innerHTML = "Whenever a creature you control " + textTrigger + ", you may " + textEffect + ".";
+      break;
+    case 3:
+      textBox.innerHTML = textAbility + "<br>" + textCost + ", TAP: " + textEffect + " and you gain 3 life.";
+      break;
+    case 4:
+      textBox.innerHTML = textAbility + "<br>If you cast a " + textType + " this turn, creatures you control get +" + textCost + "/" + textCost + ".";
+      break;
+    case 5:
+      textBox.innerHTML = textAbility + "<br>Sacrifice " + textCost + " other creatures: " + textEffect + ". Target " + textType + " gains shroud until end of turn.";
+      break;
+    case 6:
+      textBox.innerHTML = textAbility + "<br>If you control at least " + textCost + " " + textType + ", " + textType + "s you control have indestructible.";
+      break;
+    case 7:
+      textBox.innerHTML = "At the begining of your upkeep, you may cast a " + textType + " spell from your graveyard. This spell costs " + textCost + " less to cast.";
+      break;
+    case 8:
+      textBox.innerHTML = textAbility + "<br>At the beginning of your end step, you may discard a card. If you do, " + textEffect; 
+  }
+  
 }
